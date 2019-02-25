31d942112dfc6e969af0f50ea7ddd33e68abaa47
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-02-24 22:23:42 -0600

    Added 'next' button to click when submitting guess on index.html Also started basic notes in app.js for high level pseudo code. None functioning timer that subtracts one from the time value and dispplays on index.html

---
+++ assets/javascript/app.js
@@ -0,0 +1,53 @@
+// Declare variables
+
+var time = 3;
+
+
+
+// Timer that counts down to and stops at zero
+
+
+// Display question with radio buttons selection for answers, only one selectable
+
+// submit button that stores the answer in memory and displays the next trivia question. If no questions left ends game and displays results (number correct/incorrect)
+
+// When timer reaches zero display trivia quiz results (correct/incorrect) and play again button
+
+
+// $(".timerText").text(time);
+
+function countDown() {
+    if (time > 0) {
+    time--;
+    } 
+
+
+  // DONE: Get the current time, pass that into the timeConverter function,
+  //       and save the result in a variable.
+  var converted = timeConverter(time);
+  console.log(converted);
+
+  // DONE: Use the variable we just created to show the converted time in the "display" div.
+  $("#timerText").text(converted);
+}
+
+function timeConverter(t) {
+
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+  
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+  
+    if (minutes === 0) {
+      minutes = "00";
+    }
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+  
+    return minutes + ":" + seconds;
+  }
+
+countDown();
